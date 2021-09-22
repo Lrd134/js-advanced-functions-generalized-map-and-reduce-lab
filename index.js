3b7786b1e96d6606f731cf0c6cfae8801a259c55
Lrd134 <larryc3200@gmail.com>
2021-09-22 04:22:31 -0400

    commit tree 468b61e2d80350b25970e0686f47243636039c5e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(info, fn) {
+  let arr = []
+  info.forEach( e =>
+    {
+      arr.push(fn(e));
+    });
+    return arr;
+}
+
+function reduce(src, callback, value) {
+  let total;
+  if (value) {
+    total = value;
+    for (let i = 0; i < src.length; i++) {
+      total = callback(src[i], total);
+    }
+    return total;
+  } else {
+    total = src[0];
+    for (let i = 1; i < src.length; i++) {
+      total = callback(src[i], total);
+    }
+    return total;
+  }
+}
